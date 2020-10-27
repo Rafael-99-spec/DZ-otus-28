c4ced46fd6af521d64fff94a616fd5a4289b0bcf
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-10-27 14:14:33 +0200

    Create sed.sh

---
+++ files/sed.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo sed -i '25s/\[ ${NETWORKING} = "no" \] && exit 0/\[[ ${NETWORKING} = "no" \]] \&\& exit 0/' /etc/init.d/knockd
+sudo sed -i '23a\sleep 30' /etc/init.d/knockd
